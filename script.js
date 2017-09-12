19fba488aab201a45dda56123d146577da974844
Maciej <maciek.ziemba@gmail.com>
2017-09-12 14:29:33 +0200

    Enter keydown functionality and game status added.

--- script.js
+++ script.js
@@ -1,29 +1,69 @@
 /**
  * Created by maciej on 12.09.17.
  */
-var number=Math.floor(Math.random()*101);
-var guess=0; /*initializing players variables*/
-var guessesMade=0;
-var guessesRemaining=10;
+var number = Math.floor(Math.random() * 101);
+console.log(number);
+var guess = 0;
+/*initializing players variables*/
+var guessesMade = 0;
+var guessesRemaining = 10;
+var gameWon = false;
 
-var input=document.getElementById('input');
-var info=document.getElementById('info');
-var button=document.querySelector('button');
+var input = document.getElementById('input');
+var info = document.getElementById('info');
+var button = document.querySelector('button');
 
-button.style.cursor='pointer';
+button.style.cursor = 'pointer';
 button.addEventListener('click', clickHandler, false);
+window.addEventListener('keydown', keydownHandler, false);
 
-function clickHandler(){
-    startGame()
+function keydownHandler(event){
+    if (event.keyCode===13){
+        validateInput()
+    }
 }
 
-function startGame(){
+
+function clickHandler() {
+    validateInput()
+}
+
+function validateInput(){
     guess=parseInt(input.value);
-    if (guess<number){
-        info.innerHTML='The number you\'re looking for is higher...';
-    } else if (guess>number){
-        info.innerHTML='The number  you\'re looking for is lower...';
+    if (isNaN(guess)){
+        info.innerHTML='Please, enter the number - not a word.'
     } else {
-        info.innerHTML='You\'ve found it! Congratulations!';
+        startGame();
+    }
+
+}
+
+function startGame() {
+    guess = parseInt(input.value);
+    guessesMade++;
+    guessesRemaining--;
+    if (guessesRemaining < 1) {
+        endGame();
+    } else {
+        if (guess < number) {
+            info.innerHTML = 'The number you\'re looking for is higher... Guess: ' +
+                '' + guessesMade + ', Remaining: ' + guessesRemaining;
+        } else if (guess > number) {
+            info.innerHTML = 'The number  you\'re looking for is lower...';
+        } else {
+            gameWon = true;
+            endGame();
+        }
+    }
+
+}
+
+function endGame() {
+    if (gameWon) {
+        info.innerHTML = 'Congratulations, you\'ve found the number and it took you only ' + guessesMade + ' ' +
+            'steps! Thanks for playing.';
+    } else {
+        info.innerHTML = 'Unfortunatelly, you\'ve run out of chances... The number was: ' + number +
+            '. Thanks for playing.';
     }
 }
